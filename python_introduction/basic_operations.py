007bafc8b4b43a40e39074846f7d3846a305e638
Eyoel Alemu <eyualemu777@gmail.com>
2025-11-09 12:13:59 -0800

    second commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+x = number1 + number2
+y = number1 - number2
+z = number2 * number1
+print("Addition of", number1, "and", number2, "is", x)
+print("Subtraction of", number1, "and", number2, "is", y)
+print("Multiplication of", number1, "and", number2, "is", z)
